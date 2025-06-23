a73693b169a6cf5eb4c13625268d81390f4d9616
Michael Chong <michael.yc.chong@gmail.com>
2025-06-23 16:32:38 -0400

    change "ads data" example to toronto beaches
    Reasoning:
    
    * reproducible instructor-side data preparation
    * easily accessible documentation
    * more relevant to Toronto context
    * comes in a .csv file instead of a .sav file (SPSS format)

--- 03_instructional_team/slides_data_prep.R
+++ 03_instructional_team/slides_data_prep.R
@@ -8,3 +8,12 @@ library(opendatatoronto)
 council_attendance <- get_resource("b1989a8e-d69c-4b7e-85a7-c1edc2f887e9")
 
 write_csv(here("05_src/slides_data/toronto_city_council_attendance_2018-2022.csv"))
+
+# https://open.toronto.ca/dataset/toronto-beaches-observations/
+# resources <- list_package_resources("toronto-beaches-observations")
+# Toronto beaches observations - Downloaded 23 Jun 2025
+beaches_observations <- get_resource("b3a24fa4-50a8-4086-8dad-b922969f6dca") |>
+  mutate(dataCollectionDate = ymd(dataCollectionDate)) |>
+  filter(dataCollectionDate > ymd("2022-12-31"))
+
+write_csv(beaches_observations, here("05_src/slides_data/toronto_beaches_observations.csv"))
