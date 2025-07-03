67025736e4903857120cf4e6db0a65fd1a731bb7
Michael Chong <michael.yc.chong@gmail.com>
2025-07-03 17:14:44 -0400

    renovate data wrangling lesson
    * take out data.table portion into an additional (optional) lesson
    * change data examples
    * intersperse demos
    * make exercises more involved
    * integrate other skills into exercises

--- 03_instructional_team/slides_data_prep.R
+++ 03_instructional_team/slides_data_prep.R
@@ -12,8 +12,22 @@ write_csv(here("05_src/slides_data/toronto_city_council_attendance_2018-2022.csv
 # https://open.toronto.ca/dataset/toronto-beaches-observations/
 # resources <- list_package_resources("toronto-beaches-observations")
 # Toronto beaches observations - Downloaded 23 Jun 2025
-beaches_observations <- get_resource("b3a24fa4-50a8-4086-8dad-b922969f6dca") |>
+
+beaches_observations <- list_package_resources("toronto-beaches-observations") |>
+  filter(name == "toronto-beaches-observations.csv") |> 
+  get_resource() |>
   mutate(dataCollectionDate = ymd(dataCollectionDate)) |>
-  filter(dataCollectionDate > ymd("2022-12-31"))
+  filter(dataCollectionDate > ymd("2023-12-31"), dataCollectionDate < ymd("2025-01-01"))
 
 write_csv(beaches_observations, here("05_src/slides_data/toronto_beaches_observations.csv"))
+
+# https://open.toronto.ca/dataset/toronto-beaches-water-quality/
+# resources <- list_package_resources("92b0de8f-1ada-44a7-84cf-adc04868e990")
+beaches_water_quality <- list_package_resources("toronto-beaches-water-quality") |>
+  filter(name == "toronto-beaches-water-quality - 4326.csv") |>
+  get_resource() |>
+  mutate(collectionDate = ymd(collectionDate)) |>
+  filter(collectionDate > ymd("2023-12-31"), collectionDate < ymd("2025-01-01")) |>
+  select(-geometry)
+
+write_csv(beaches_water_quality, here("05_src/slides_data/toronto_beaches_water_quality.csv"))
